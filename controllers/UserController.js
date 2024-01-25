049d343d46aad913764d74b20cd1a7cdd6c1f841
Mohit Kumar <mohit2991kumar@gmail.com>
2024-01-25 15:52:37 +0530

    add

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,19 @@
 // controllers/userController.js
 const UserModel = require('../models/userModel');
+const nodemailer = require('nodemailer');
 
 const UserController = {
   getAllUsers: (req, res) => {
-    UserModel.getAllUsers((err, results) => {
-      if (err) {
-        return res.status(500).json({ error: 'Internal Server Error' });
+    // get all users
+    const users = UserModel.getAllUsers((err, result) => {
+      if(err){
+        return res.status(500).json({error: "internul server error"})
       }
-      res.json(results);
+
+      return res.status(200).json({userlist:result})
+
     });
+    // console.log(">>>>>>>............ users", users);
   },
 
   getUserById: (req, res) => {
@@ -29,6 +34,57 @@ const UserController = {
       }
       res.json({ id: results.insertId });
     });
+  },
+
+  loginUser: (req, res) => {
+    const { email, password } = req.body;
+
+    UserModel.loginUser(email, password, (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+      res.json({ message: 'Login successful', user });
+    });
+  },
+
+
+  forgotPassword: (req, res) => {
+    const { email } = req.body;
+
+ 
+    UserModel.getUserByEmail(email, (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      if (!user) {
+        
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      const resetToken = crypto.randomBytes(20).toString('hex');
+      const resetTokenExpiry = Date.now() + 3600000; 
+
+      UserModel.updateResetToken(user.id, resetToken, resetTokenExpiry, (updateError, updateResults) => {
+        if (updateError) {
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const resetLink = `http://your-app/reset-password?token=${resetToken}`;
+        sendPasswordResetEmail(email, resetLink);
+
+        res.json({ message: 'Password reset instructions sent to your email' });
+      });
+    });
+  },
+
+  updateUserdetails: (req, res) => {
+    const userData = req.body;
+    const dd = UserModel.updateUserdetails(userData);
+    console.log(">>>>>>>> mmohit", dd);
   },
 
   // Add other CRUD methods as needed
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,8 @@
 // models/userModel.js
+const bcrypt = require('bcrypt');
 const db = require('../config/db');
+const crypto = require('crypto');
+const moment = require('moment');
 
 const UserModel = {
   getAllUsers: (callback) => {
@@ -11,10 +14,79 @@ const UserModel = {
   },
 
   createUser: (userData, callback) => {
-    db.query('INSERT INTO users SET ?', userData, callback);
+    const { name, email, password } = userData;
+
+    bcrypt.hash(password, 10, (hashError, hashedPassword ) => {
+      if (hashError) {
+        return callback(hashError, null);
+      }
+
+      db.query('INSERT INTO users(name, email, password) VALUES (?, ?,?)', [name, email, hashedPassword ], (error, results) => {
+
+        if (error) {
+          return callback(error, null);
+        };
+        return callback(null, { id: results.inse });
+      });
+    });
   },
 
-  // Add other CRUD methods as needed
+  loginUser: (email, password, callback) => {
+  
+    db.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      if (results.length === 0) {
+     return callback(null, null);
+      }
+
+      const user = results[0];
+
+      bcrypt.compare(password, user.password, (bcryptError, isMatch) => {
+        if (bcryptError) {
+          return callback(bcryptError, null);
+        }
+
+        if (!isMatch) {
+          return callback(null, null);
+        }
+        const { password, ...userData } = user;
+        return callback(null, userData);
+      });
+    });
+  },
+
+  getUserByEmail: (email, callback) => {
+    db.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      if (results.length === 0) {
+      
+        return callback(null, null);
+      }
+
+      const user = results[0];
+      return callback(null, user);
+    });
+  },
+
+  updateResetToken: (userId, resetToken, resetTokenExpiry, callback) => {
+    db.query('UPDATE users SET reset_token = ?, reset_token_expiry = ? WHERE id = ?', [resetToken, resetTokenExpiry, userId], (error, results) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      return callback(null, results);
+    });
+  },
+
+  updateUserdetails: (userData, callback) => {
+    return db.query('INSERT INTO userdetails SET ?', userData);
+  }
 };
 
 module.exports = UserModel;
